0e26881e1a851bd9173738bd4fb55d4855ba6ab2
flparvez <flparvez23@gmail.com>
2024-03-12 11:45:36 +0600

    test fetching data success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Featured from "@/components/featured/Featured";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>hellow</div>;
+  return (
+    <div>
+      hellow
+      <Featured />
+    </div>
+  );
 }
